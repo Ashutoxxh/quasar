284d23abd299e8dcc227e769dba4d6795af1b626
Ashutoxxh <kbcincashutosh@gmail.com>
2023-04-07 23:38:31 +0530

    quasar project set up

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page class="flex flex-center">
+    <logInComponent />
+  </q-page>
+</template>
+
+<script setup>
+import logInComponent from "src/components/logInComponent.vue";
+</script>
